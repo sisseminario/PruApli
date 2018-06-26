d78f51c57e4f917636af9f2b3ac617d747fd9685
Ele2807 <sis015melenataboada@gmail.com>
2018-06-26 13:03:26 -0400

    para acualizar listas

--- app/src/main/java/com/example/elena/eden/Contactos.java
+++ app/src/main/java/com/example/elena/eden/Contactos.java
@@ -1,25 +1,28 @@
 package com.example.elena.eden;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class Contactos extends AppCompatActivity  {
-    EditText nom,corre,pass;
-    Button resgis;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contactos);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        updateUI();
-    }
-    private void updateUI(){
-        nom =(EditText) findViewById(R.id.id_nom);
-        corre=(EditText) findViewById(R.id.id_corre);
-        pass=(EditText) findViewById(R.id.id_pass);
-        resgis=(Button) findViewById(R.id.id_regis);
+
+
 
     }
- }
+   }
