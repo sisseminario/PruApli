f4ed25d8d54d7fe78e8c06a61739a88b1414bf30
valeria007 <sis015abigail@gmail.com>
2018-07-09 11:21:09 -0400

    commit llamada

--- app/src/main/java/com/example/elena/eden/DetallesCasa.java
+++ app/src/main/java/com/example/elena/eden/DetallesCasa.java
@@ -1,11 +1,14 @@
 package com.example.elena.eden;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -18,6 +21,8 @@ import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.net.URI;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -46,6 +51,26 @@ public class DetallesCasa extends AppCompatActivity {
 
         Toast.makeText(this, idCasa, Toast.LENGTH_SHORT).show();
         //id= this.getIntent().getExtras().getString("id");
+
+        //LLAMADAS//
+
+        FloatingActionButton mDialButton = (FloatingActionButton) findViewById(R.id.llamada);
+        final TextView mPhoneNoEt = (TextView) findViewById(R.id.textView90);
+
+
+        mDialButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNo = mPhoneNoEt.getText().toString();
+                if(!TextUtils.isEmpty(phoneNo)) {
+                    String dial = "tel:" + phoneNo;
+                    startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(dial)));
+                }else {
+                    Toast.makeText(DetallesCasa.this, "Enter a phone number", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 
 
